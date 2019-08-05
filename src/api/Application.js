20d086a611bcf164f381224205d4e64b05f7300a
guoxunbo <11603652@qq.com>
2019-08-05 14:07:28 +0800

    提交1.0.1版本

--- src/api/Application.js
+++ src/api/Application.js
@@ -1,6 +1,6 @@
 const Application = {
     name: '智行管理系统',
-    version: "1.0.0",
+    version: "1.0.1",
     copyright: {
         name: "© 2018-2019 By Newbiest",
         url: "https://github.com/guoxunbo/newbiest/"
