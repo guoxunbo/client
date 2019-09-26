07eea1822cb5b2a0eb5b7ec6baf80d3c21dd9993
guoxunbo <11603652@qq.com>
2019-09-26 16:38:27 +0800

    #72
    close #72

--- src/pages/Properties/components/GcMaterialLotStockInProperties.jsx
+++ src/pages/Properties/components/GcMaterialLotStockInProperties.jsx
@@ -5,7 +5,7 @@ import StockInManagerRequest from "../../../api/gc/stock-in/StockInManagerReques
 /**
  * 物料批次扫描添加后入库
  *  只有一个扫描框，根据不同的开头做不同的事。
- * 先扫描一些ID（此时页面只显示ID，不带出其他信息）扫描到MB开头，则临时记录下来中转箱ID；扫描到ZHJ/HJ开头，则临时记录下货架号；
+ * 先扫描一些ID 此时页面只显示ID，不带出其他信息）扫描到MB开头，则临时记录下来中转箱ID；扫描到ZHJ/HJ开头，则临时记录下货架号；其他都是物料批次号
  * ——新的记录节点
  * 只要扫到不是MB/ZHJ/HJ开头ID，则认为是新的开始（之前记录的数据不再修改）
  */
@@ -13,38 +13,55 @@ export default class GcMaterialLotStockInProperties extends EntityScanProperties
 
     static displayName = 'GcMaterialLotStockInProperties';
     
+    constructor(props) {
+        super(props);
+        this.state = {...this.state, ...{currentHandleMLots:[], scanRelaxBoxOrStorageFlag: false}};
+      }
+
     handleSearch = () => {
         let self = this;
-        let {rowKey,tableData} = this.state;
+        let {rowKey,tableData, scanRelaxBoxOrStorageFlag, currentHandleMLots} = this.state;
         this.setState({loading: true});
         let data = "";
         let queryFields = this.form.state.queryFields;
         if (queryFields.length === 1) {
             data = this.form.props.form.getFieldValue(queryFields[0].name)
         }  
-        debugger;
+         // MB开头的则是中装箱号 扫描到MB开头的，则更新当前操作的物料批次的中装箱号
+        let dataIndex = -1;
         if (data.startsWith("MB")) {
-            // MB开头的则是中装箱号 扫描到MB开头的，则更新表格中中转箱号为空的批次的中转箱号为此中装箱号
-            tableData.forEach((materialLot) => {
-                if (materialLot["relaxBoxId"] === undefined) {
-                    materialLot["relaxBoxId"] = data;
-                }
+            debugger;
+            console.log(currentHandleMLots);
+            currentHandleMLots.forEach((materialLot) => {
+                tableData.map((data, index) => {
+                    if (data[rowKey] == materialLot[rowKey]) {
+                        dataIndex = index;
+                    }
+                });
+                materialLot["relaxBoxId"] = data;
+                tableData.splice(dataIndex, 1, materialLot);
             });
             self.setState({ 
                 tableData: tableData,
-                loading: false
+                loading: false,
+                scanRelaxBoxOrStorageFlag: true,
             });
             self.form.resetFormFileds();
         } else if (data.startsWith("ZHJ") || data.startsWith("HJ") ) {
-            // ZHJ/HJ 开头的则是库位号 扫描到ZHJ/HJ开头的，则更新表格中库位号为空的库位号为此库位号
-            tableData.forEach((materialLot) => {
-                if (materialLot["storageId"] === undefined) {
-                    materialLot["storageId"] = data;
-                }
+            // ZHJ/HJ 开头的则是库位号 扫描到ZHJ/HJ开头的，则更新当前操作的物料批次的库位号
+            currentHandleMLots.forEach((materialLot) => {
+                tableData.map((data, index) => {
+                    if (data[rowKey] == materialLot[rowKey]) {
+                        dataIndex = index;
+                    }
+                });
+                materialLot["storageId"] = data;
+                tableData.splice(dataIndex, 1, materialLot);
             });
             self.setState({ 
                 tableData: tableData,
-                loading: false
+                loading: false,
+                scanRelaxBoxOrStorageFlag: true,
             });
             self.form.resetFormFileds();
         } else {
@@ -52,13 +69,21 @@ export default class GcMaterialLotStockInProperties extends EntityScanProperties
             let requestObject = {
                 materialLotId: data,
                 success: function(responseBody) {
+                    debugger;
                     let materialLot = responseBody.materialLot;
                     if (tableData.filter(d => d[rowKey] === materialLot[rowKey]).length === 0) {
                         tableData.unshift(materialLot);
                     }
+                    // 如果扫描过中装箱号或者库位号，则表示上次操作的批次已经结束，作为新起点继续下次操作
+                    if (scanRelaxBoxOrStorageFlag) {
+                        currentHandleMLots = []; 
+                    }
+                    currentHandleMLots.push(materialLot);
                     self.setState({ 
                         tableData: tableData,
-                        loading: false
+                        loading: false,
+                        scanRelaxBoxOrStorageFlag: false,
+                        currentHandleMLots: currentHandleMLots
                     });
                     self.form.resetFormFileds();
                 },
