ec5a6974dd766fc5f5186a44e0ed1502d4a395bb
guozhang.luo <1972685455@qq.com>
2019-10-12 14:34:07 +0800

    #W047  重测发料
    cloes #W047  重测发料先扫描验证 ，后选择订单重测

--- src/pages/Properties/components/GcReTestOrderMLotProperties.jsx
+++ src/pages/Properties/components/GcReTestOrderMLotProperties.jsx
@@ -2,6 +2,7 @@ import EntityScanProperties from "./entityProperties/EntityScanProperties";
 import GcReTestMLotTable from "../../../components/Table/gc/GcReTestMLotTable";
 import TableManagerRequest from "../../../api/table-manager/TableManagerRequest";
 import ValidationSoOrTestRequest from "../../../api/gc/validation-so-test/ValidationSoOrTestRequest";
+import ValidationMaterialRequest from "../../../api/gc/validation -material/ValidationMaterialRequest";
 
 export default class GcReTestOrderMLotProperties extends EntityScanProperties{
 
@@ -24,13 +25,7 @@ export default class GcReTestOrderMLotProperties extends EntityScanProperties{
 
     queryData = (whereClause) => {
         const self = this;
-        let documentLine = this.props.orderTable.getSingleSelectedRow();
-        if (!documentLine) {
-            self.setState({ 
-                loading: false
-            });
-            return;
-        }
+        let {rowKey,tableData} = this.state;
         let requestObject = {
           tableRrn: this.state.tableRrn,
           whereClause: whereClause,
@@ -38,21 +33,56 @@ export default class GcReTestOrderMLotProperties extends EntityScanProperties{
             let queryDatas = responseBody.dataList;
             if (queryDatas && queryDatas.length > 0) {
               let data = queryDatas[0];
-              self.validationRule(documentLine, data);
-              // if (tableData.filter(d => d[rowKey] === data[rowKey]).length === 0) {
-              //   tableData.unshift(data);
-              // }
-              // self.setState({ 
-              //   tableData: tableData,
-              //   loading: false
-              // });
-              // self.form.resetFormFileds();
+              if(tableData && tableData.length > 0){
+                let materialFirst = tableData[0];
+                self.validationMaterialRule(materialFirst, data);
+              } else {
+                queryDatas.forEach(data => {
+                  if (tableData.filter(d => d[rowKey] === data[rowKey]).length === 0) {
+                    tableData.unshift(data);
+                  }
+                });
+                self.setState({ 
+                  tableData: tableData,
+                  loading: false
+                });
+                self.form.resetFormFileds();
+              }
             } else {
               self.showDataNotFound();
             }
           }
         }
         TableManagerRequest.sendGetDataByRrnRequest(requestObject);
+    }
+
+    /**
+     * 20191011 gc要求扫描箱信息时以箱信息进行对比验证
+     */
+    validationMaterialRule = (materialLotFirst, materialLot) => {
+      let self = this;
+      let {rowKey,tableData} = this.state;
+      let requestObject = {
+        materialLotFirst : materialLotFirst,
+        materialLot : materialLot,
+        success: function(responseBody) {
+          if (tableData.filter(d => d[rowKey] === materialLot[rowKey]).length === 0) {
+            tableData.unshift(materialLot);
+          }
+          self.setState({ 
+            tableData: tableData,
+            loading: false
+          });
+          self.form.resetFormFileds();
+        },
+        fail: function() {
+          self.setState({ 
+              loading: false
+          });
+          self.form.resetFormFileds();
+        }
+      }
+      ValidationMaterialRequest.sendValidationRequest(requestObject);
     }
 
     validationRule = (documentLine, materialLot) => {
