d89090469a5857f2aaf1e9a179599fe481f5a7ac
zt <1943896827@qq.com>
2020-12-26 10:14:53 +0800

    Revert "来料导入和接收"
    This reverts commit 306c1de7f274a8713a44b272408332368da649e7.

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -35,8 +35,6 @@ import MaterialStatusModelProperties from '@properties/mms/MaterialStatusModelPr
 import MaterialProperties from '@properties/mms/MaterialProperties';
 import MaterialLotStockInProperties from '@properties/mms/MaterialLotStockInProperties';
 import MaterialLotInventoryProperties from '@properties/mms/MaterialLotInventoryProperties';
-import IncomingMaterialDocumentLineProperties from '@properties/mms/IncomingMaterialDocumentLineProperties';
-import IncomingMaterialImportProperties from '@properties/mms/IncomingMaterialImportProperties';
 
 //gc
 import GcCheckProperties from '@properties/gc/GcCheckProperties';
@@ -180,17 +178,6 @@ const routerConfig = [
     layout: HeaderAsideFooterResponsiveLayout,
     component: EntityProperties,
   },
-  {
-    path: buildPath('MMS/IncomingMaterialDocumentLine'),
-    layout: HeaderAsideFooterResponsiveLayout,
-    component: IncomingMaterialDocumentLineProperties,
-  },
-  {
-    path: buildPath('MMS/IncomingMaterialImport'),
-    layout: HeaderAsideFooterResponsiveLayout,
-    component: IncomingMaterialImportProperties,
-  },
- 
   //MES成品接收
   {
     path: buildPath('MMS/MESFinishGoodManager'),
