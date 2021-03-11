b47cf4e9c497e08b5499a5831167c7ecc75b8cbb
guozhang.luo <1972685455@qq.com>
2021-03-11 10:02:16 +0800

    称重 #351
    箱号称重，多箱称重与单箱称重共用一个扫描框

--- src/pages/Properties/components/GcMaterialLotWeighProperties.jsx
+++ src/pages/Properties/components/GcMaterialLotWeighProperties.jsx
@@ -20,32 +20,43 @@ export default class GcMaterialLotWeighProperties extends EntityScanProperties {
         let {rowKey, tableData, currentHandleMLots} = this.state;
         this.setState({loading: true});
         let data = "";
-        let boxsWeight = "";
         let queryFields = this.form.state.queryFields;
         data = this.form.props.form.getFieldValue(queryFields[0].name);
-        boxsWeight = this.form.props.form.getFieldValue(queryFields[1].name);
         let dataIndex = -1;
-        if((data == undefined || data == "") && boxsWeight == undefined) {
+        if((data == undefined || data == "")) {
             Notification.showInfo(I18NUtils.getClientMessage(i18NCode.SearchFieldCannotEmpty));
             self.setState({ 
                 tableData: tableData,
                 loading: false
             });
-        } else if(data != undefined && boxsWeight != undefined){
-            Notification.showInfo(I18NUtils.getClientMessage(i18NCode.ThereScanFieldCanOnlyBeOne));
-            self.setState({ 
-                tableData: tableData,
-                loading: false
-            });
-            self.form.resetFormFileds();
-            this.form.state.queryFields[0].node.focus();
-            // 如果扫描的是第一个扫描框，并且扫描的是数字，则更新当前列表中没有添加重量的箱号
+           // 如果扫描的是数字，则更新当前列表中没有添加重量的箱号，存在多箱没有重量时，给多箱称重标记
         } else if (data != undefined &&  data.startsWith("0000")  && parseFloat(data).toString() != "NaN") {
+            if (tableData.length == 0){
+                Notification.showInfo(I18NUtils.getClientMessage(i18NCode.AddAtLeastOneRow));
+            }
             currentHandleMLots = this.getNotScanWeightMaterialLots(tableData);
             if(currentHandleMLots.length == 0){
                 Notification.showInfo(I18NUtils.getClientMessage(i18NCode.AddOneRowPlease));
             } else if(currentHandleMLots.length >= 2){
-                Notification.showInfo(I18NUtils.getClientMessage(i18NCode.CaseWeightNotScanned));
+                let boxsWeight = parseInt(data,10)/10000;
+                tableData.forEach((materialLot) => {
+                    currentHandleMLots.map((data, index) => {
+                        if (data[rowKey] == materialLot[rowKey]) {
+                            dataIndex = index;
+                            materialLot["weight"] = boxsWeight.toFixed(3);
+                            materialLot["boxsWeight"] = boxsWeight.toFixed(3);
+                            materialLot["boxsWeightFlag"] = 1;
+                            if(materialLot.theoryWeight){
+                                let floatValue = materialLot.floatValue;
+                                let disWeight = Math.abs(materialLot.weight - materialLot.theoryWeight);
+                                if(disWeight > floatValue){
+                                    materialLot.errorFlag = true;
+                                }
+                            }
+                            tableData.splice(dataIndex, 1, materialLot);
+                        }
+                    });
+                });
             } else {
                 data = parseInt(data,10)/10000;
                 currentHandleMLots.forEach((materialLot) => {
@@ -72,43 +83,6 @@ export default class GcMaterialLotWeighProperties extends EntityScanProperties {
                 loading: false,
             });
             self.form.resetFormFileds();
-            //如果扫描的是多箱称重，则更新列表中所有箱号的重量
-        } else if (boxsWeight != undefined) {
-            if(parseFloat(boxsWeight).toString() == "NaN"){
-                Notification.showInfo(I18NUtils.getClientMessage(i18NCode.WeightMustBeNumber));
-            } else {
-                currentHandleMLots = this.getNotScanWeightMaterialLots(tableData);
-                if (tableData.length == 0){
-                    Notification.showInfo(I18NUtils.getClientMessage(i18NCode.AddAtLeastOneRow));
-                } else if(currentHandleMLots.length != tableData.length){
-                    Notification.showInfo(I18NUtils.getClientMessage(i18NCode.AllBoxWeightMustBeEmpty));
-                } else {
-                    boxsWeight = parseInt(boxsWeight,10)/10000;
-                    tableData.forEach((materialLot) => {
-                        tableData.map((data, index) => {
-                            if (data[rowKey] == materialLot[rowKey]) {
-                                dataIndex = index;
-                                materialLot["weight"] = boxsWeight.toFixed(3);
-                                materialLot["boxsWeightFlag"] = 1;
-                                if(materialLot.theoryWeight){
-                                    let floatValue = materialLot.floatValue;
-                                    let disWeight = Math.abs(materialLot.weight - materialLot.theoryWeight);
-                                    if(disWeight > floatValue){
-                                        materialLot.errorFlag = true;
-                                    }
-                                }
-                                tableData.splice(dataIndex, 1, materialLot);
-                            }
-                        });
-                    });
-                }
-            }
-            self.setState({ 
-                tableData: tableData,
-                loading: false,
-            });
-            self.form.resetFormFileds();
-        //如果扫描的是第一个扫描框，并且扫描的不是数字"0000"开头
         } else if (data != undefined){
             let requestObject = {
                 materialLotId: data,
