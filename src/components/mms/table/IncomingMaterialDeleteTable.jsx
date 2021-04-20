783b958253a03475c446611294678f3704ef8d7e
zt <1943896827@qq.com>
2021-04-20 23:40:24 +0800

    Revert "1"
    This reverts commit 37dd86a791804c233128d964809b9d12688edbb2.

--- src/components/mms/table/IncomingMaterialDeleteTable.jsx
+++ src/components/mms/table/IncomingMaterialDeleteTable.jsx
@@ -18,7 +18,7 @@ export default class IncomingMaterialDeleteTable extends EntityListTable {
     }
 
     buildOperationColumn = () => {
-        return
+        
     }
 
     createDeleteRemarkInput = () => {
