5ccdf2f1cc3cc2e79f07a85e583b0b68219b034a
Brave007 <807476714@qq.com>
2021-07-07 16:36:44 +0800

    修改提交
    重新修改提交

--- src/api/utils/MessageUtils.ts
+++ src/api/utils/MessageUtils.ts
@@ -1,66 +1,61 @@
+import React from "react";
 import {ResultIdentify, Language, UrlConstant} from '@const/ConstDefine';
+
 import NoticeUtils from '@utils/NoticeUtils';
+import {Response} from '@api/Response';
 
-import {Response} from "@api/Response";
-import {ResponseHeader} from "@api/ResponseHeader";
-
-import axios from "axios";
-import { SessionContext, Application } from '@api/Application';
+import {ResponseHeader} from '@api/ResponseHeader';
+import {SessionContext} from '@api/Application';
 import I18NUtils from '@utils/I18NUtils';
 import EventUtils from '@utils/EventUtils';
-import { i18NCode } from '@const/i18n';
-import fetchJsonp from 'fetch-jsonp';
+import {i18NCode} from '@const/i18n';
 import MessageRequestBody from '@api/message-manager/MessageRequestBody';
 import MessageRequestHeader from '@api/message-manager/MessageRequestHeader';
-import Request from '@api/Request';
+import Request from "@api/Request";
+import {Fetch} from '@const/axis'
+
 /**
  *  消息主要发送类
  */
-export default class MessageUtils {
-    
+
+export default function MessageUtils():React.ReactNode{
+
     /**
      * 同时发送2个请求，并且都处理完毕一起返回
      * 常用场景比如用户组获取用户和获取所有用户一起使用
      * @param requestObject
      * @example {requests: [request1, request2...], success:}
      */
-    static sendTwoRequest(requestObject) {
-        let self = this;
+     const  sendTwoRequest= async (requestObject: any) => {
         let requests = requestObject.requests;
         if (Array.isArray(requests)) {
-            let axioses = [];
-            requests.forEach((requestObject)=>{
+            let axioses: any = [];
+            requests.forEach((requestObject) => {
                 let request = requestObject.request;
-                axioses.push(axios.post(request.url, request, {
-                    headers:{
-                        authorization: SessionContext.getToken()
-                    }
-                }));
+                axioses.push(Fetch(request.url, 'post', request));
             });
-            axios.all(axioses).then(axios.spread(function(responseValue1, responseValue2) {
-                // 处理2个reponse 都成功才回调Object.success
+            await Promise.all(axioses).then(([responseValue1, responseValue2]: any) => {
                 let response1 = new Response(responseValue1.data.header, responseValue1.data.body);
                 let response2 = new Response(responseValue2.data.header, responseValue2.data.body);
                 if (ResultIdentify.Fail == response1.header.result) {
-                    self.handleException(response1.header);
+                    handleException(response1.header);
                     return;
-                } 
+                }
                 if (ResultIdentify.Fail == response2.header.result) {
-                    self.handleException(response2.header);
+                    handleException(response2.header);
                     return;
-                } 
+                }
                 if (requestObject.success) {
                     requestObject.success(response1.body, response2.body);
                 } else {
                     NoticeUtils.showSuccess();
                 }
-            })).catch(function(exception) {
-                self.handleException(exception);
+            }).catch(function (exception) {
+                handleException(exception);
             });
-        } else {
-
         }
-    }
+    };
+
 
     /**
      * 发送导入请求
@@ -68,24 +63,22 @@ export default class MessageUtils {
      * @param requestObject {url:"", request:{param1, param2},}
      * @param file 文件
      */
-    static sendImportData(requestObject, file) {
-        let self = this;
+    const sendImportData = (requestObject: any, file: any): void => {
         let formData = new FormData();
-        formData.append("file", file);
+        formData.append('file', file);
         let request = requestObject.request;
         for (let propName in requestObject) {
-            if (propName === "request") {
+            if (propName === 'request') {
                 formData.append(propName, JSON.stringify(request));
             } else {
                 formData.append(propName, requestObject[propName]);
             }
         }
-        axios.post(request.url, formData, {
-            headers: { 'Content-Type': 'multipart/form-data',  authorization: SessionContext.getToken() }
-        }).then(function(object) {
+        try {
+            const object:any = Fetch(request.url,'ImportData', formData);
             let response = new Response(object.data.header, object.data.body);
             if (ResultIdentify.Fail == response.header.result) {
-                self.handleException(response.header);
+                handleException(response.header);
             } else {
                 if (requestObject.success) {
                     requestObject.success(response.body);
@@ -93,11 +86,10 @@ export default class MessageUtils {
                     NoticeUtils.showSuccess();
                 }
             }
-        }).catch(function(exception) {
-            self.handleException(exception);
-        }); 
-
-    }
+        }catch (exception){
+            handleException(exception);
+        }
+    };
 
     /**
      * 发送导出数据请求比如导出excel 具体类型由contnt-type决定
@@ -105,23 +97,18 @@ export default class MessageUtils {
      * @param requestObject {url:"", request:{param1, param2},}
      * @param fileName 文件名字
      */
-    static sendExpRequest(requestObject, fileName) {
-        let self = this;
+    const sendExpRequest = (requestObject: any, fileName: any): void => {
         let request = requestObject.request;
-        axios.post(request.url, request, {
-            responseType: 'blob',
-            headers: {
-                authorization: SessionContext.getToken()
-            }
-        }).then(function(object) {
+        try {
+            const object:any = Fetch(request.url,'ExpExcel', request);
             let type = object.headers['content-type'];
-            let blob = new Blob([object.data], { type: type}); 
+            let blob = new Blob([object.data], {type: type});
             let reader = new FileReader();
-            reader.onload = e => {
-                if (e.target.result.indexOf("result") != -1) {
+            reader.onload = (e: any) => {
+                if (e.target.result.indexOf('result') != -1) {
                     let result = JSON.parse(e.target.result);
                     let response = new Response(result.header, result.body);
-                    self.handleException(response.header);
+                    handleException(response.header);
                 } else {
                     let elink = document.createElement('a');
                     elink.download = fileName;
@@ -132,63 +119,51 @@ export default class MessageUtils {
                     document.body.removeChild(elink);
                     NoticeUtils.showSuccess();
                 }
-            }
+            };
             reader.readAsText(blob);
             EventUtils.sendButtonLoaded();
-        }).catch(function(exception) {
-            self.handleException(exception);
-        }); 
-    }
+        }catch (exception){
+            handleException(exception);
+        }
+    };
 
     /**
      * 发送同步请求 需要配合await使用
-     * @param {} requestObject 
+     * @param {} requestObject
      * @return Promise对象。取出里面的值需要通过Promise.resolve取出
      */
-    static async sendSyncRequest(requestObject) {
-        let self = this;
+    const sendSyncRequest = async (requestObject: any) => {
         let request = requestObject.request;
-        let timeOut = request.timeOut || Application.timeOut;
-        return axios.post(request.url, request, {
-            timeout: timeOut,
-            headers:{
-                authorization: SessionContext.getToken(),
-            }
-        }).then(function(object) {
+        try {
+            const object:any =  await Fetch(request.url,'post', request);
             let response = new Response(object.data.header, object.data.body);
             if (ResultIdentify.Fail == response.header.result) {
                 if (requestObject.fail) {
-                    requestObject.fail();
+                    return requestObject.fail();
                 }
-                self.handleException(response.header);
+                handleException(response.header);
             } else {
                 EventUtils.sendButtonLoaded();
                 return response.body;
             }
-        }).catch(function(exception) {
-            self.handleException(exception);
-        }); 
-    }
+        }catch (exception){
+            handleException(exception);
+        }
+    };
 
     /**
      * 发送异步请求
      */
-    static sendRequest(requestObject) {
-        let self = this;
+    const sendRequest = async (requestObject: any) => {
         let request = requestObject.request;
-        let timeOut = request.timeOut || Application.timeOut;
-        axios.post(request.url, request, {
-            timeout: timeOut,
-            headers:{
-                authorization: SessionContext.getToken(),
-            }
-        }).then(function(object) {
+        try {
+            const object: any = await Fetch(request.url, 'post', request);
             let response = new Response(object.data.header, object.data.body);
             if (ResultIdentify.Fail == response.header.result) {
                 if (requestObject.fail) {
-                    requestObject.fail();
+                  requestObject.fail();
                 }
-                self.handleException(response.header);
+                handleException(response.header);
             } else {
                 if (object.headers.authorization) {
                     SessionContext.saveToken(object.headers.authorization);
@@ -200,62 +175,40 @@ export default class MessageUtils {
                 }
                 EventUtils.sendButtonLoaded();
             }
-        }).catch(function(exception) {
-            self.handleException(exception);
-        }); 
-    }
+        } catch (exception) {
+            handleException(exception);
+        }
+    };
 
     /**
      * 发送Get请求
      *  一般用于打印机等
      */
-    static sendGetRequest(requestObject) {
-        let self = this;
-        axios.get(requestObject.url, {
-            params: requestObject.params
-        }).then(function(object) {
+    const sendGetRequest = async (requestObject: any) => {
+        try {
+            const object:any = await Fetch(requestObject.url,'get', requestObject.params);
             if (requestObject.success) {
                 requestObject.success(object.data);
             } else {
                 NoticeUtils.showSuccess();
             }
-        }).catch(function(exception) {
-            self.handleException(exception);
-        }); 
-    }
+        }catch(exception){
+            handleException(exception);
+        }
+    };
 
     /**
-     * 发送Jsonp请求
-     *  
+     * 异常返回提示
      */
-    static sendJsonpRequest(requestObject) {
-        let url = requestObject.url;
-        let params = requestObject.params;
-        let paramFlag= url.indexOf('?') === -1 ? '?' : '&';
-        url += `${paramFlag}`;
-        if (params) {
-            for (let i in params) {
-                url += `${i}=${params[i]}&`
-            }
-        }
-        url = url.substring(0, url.length-1); 
-        fetchJsonp(url).then(function(response) {
-            console.log(response);
-        }).catch(function(ex) {
-            console.log('parsing failed', ex)
-        })
-    }
-
-    static async handleException(exception) {
+     function  handleException(exception:any){
         if (exception.response) {
             console.log(exception.response);
             if (exception.response.status === 401) {
                 EventUtils.sendTokenError();
                 return;
-            } 
+            }
         }
-
-        let error = "";
+        let error = '';
         let errroCode = 0;
         let language = SessionContext.getLanguage();
         if (!language) {
@@ -267,8 +220,10 @@ export default class MessageUtils {
             let requestHeader = new MessageRequestHeader();
             let requestObject = {
                 request: new Request(requestHeader, requestBody, UrlConstant.MessageManagerUrl),
-            }
-            let responseBody = await this.sendSyncRequest(requestObject);
+            };
+            let responseBody: any = async () => {
+                await sendSyncRequest(requestObject);
+            };
             if (responseBody.message) {
                 if (language == Language.Chinese) {
                     error = responseBody.message.messageZh;
@@ -276,21 +231,32 @@ export default class MessageUtils {
                     error = responseBody.message.message;
                 }
                 errroCode = responseBody.message.objectRrn;
-            } 
+            }
         } else {
             let errorMessage = exception.message;
             // String的不是后台的错误 需要去加载Client端的i18N信息
-            if (errorMessage === "Network Error") {
+            if (errorMessage === 'Network Error') {
                 error = I18NUtils.getClientMessage(i18NCode.NetworkError);
-            } else if (errorMessage.indexOf("timeout") != -1) {
+            } else if (errorMessage.indexOf('timeout') != -1) {
                 error = I18NUtils.getClientMessage(i18NCode.TimeOut);
             } else {
                 console.error(exception);
-                error = exception;
+                error = exception.message;
             }
         }
         NoticeUtils.showError(errroCode, error);
         EventUtils.sendButtonLoaded();
-    }
-}
+    };
 
+
+
+    return {
+        sendTwoRequest,
+        sendImportData,
+        sendExpRequest,
+        sendSyncRequest,
+        sendRequest,
+        sendGetRequest,
+    }
+
+}
