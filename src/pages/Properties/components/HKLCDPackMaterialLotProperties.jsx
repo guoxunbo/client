c0bb53ea5899dedcbcdd9de8807f8d12dc0b3bda
guozhangLuo <55224413+guozhangluo@users.noreply.github.com>
2022-06-21 18:53:21 +0800

    香港仓LCD装箱

---
+++ src/pages/Properties/components/HKLCDPackMaterialLotProperties.jsx
@@ -0,0 +1,77 @@
+import EntityScanProperties from "./entityProperties/EntityScanProperties";
+import TableManagerRequest from "../../../api/table-manager/TableManagerRequest";
+import PackageValidationRequest from "../../../api/package-validation/PackageValidationRequest";
+import HKLCDPackMaterialLotTable from "../../../components/Table/gc/HKLCDPackMaterialLotTable";
+
+const PackageType = "HKLCDPackCase";
+
+export default class HKLCDPackMaterialLotProperties extends EntityScanProperties{
+
+    static displayName = 'HKLCDPackMaterialLotProperties';
+    
+    queryData = (whereClause) => {
+        const self = this;
+        let requestObject = {
+          tableRrn: this.state.tableRrn,
+          whereClause: whereClause,
+          success: function(responseBody) {
+            let queryDatas = responseBody.dataList;
+            if (queryDatas && queryDatas.length > 0) {
+              self.validationPackgeRule(queryDatas[0]);
+            } else {
+              self.showDataNotFound();
+            }
+          }
+        }
+        TableManagerRequest.sendGetDataByRrnRequest(requestObject);
+    }
+
+    validationPackgeRule(materialLot) {
+        let self = this;
+        let {rowKey,tableData} = this.state;
+        if (tableData.filter(d => d[rowKey] === materialLot[rowKey]).length === 0) {
+            tableData.unshift(materialLot);
+        }
+        if (tableData.length == 0) {
+            self.setState({ 
+                tableData: tableData,
+                loading: false
+            });
+            self.form.resetFormFileds();
+        } else {
+            let requestObject = {
+                packageType: PackageType,
+                materialLots: tableData,
+                success: function() {
+                    self.setState({ 
+                        tableData: tableData,
+                        loading: false
+                    });
+                    self.form.resetFormFileds();
+                },
+                fail: function() { 
+                    tableData.shift();
+                    self.setState({ 
+                        tableData: tableData,
+                        loading: false
+                    });
+                    self.allFieldBlur();
+                }
+            }
+            PackageValidationRequest.sendValidationPackRequest(requestObject);
+        }
+        
+    }
+
+    buildTable = () => {
+        return <HKLCDPackMaterialLotTable pagination={false} 
+                                    rowKey={this.state.rowKey} 
+                                    selectedRowKeys={this.state.selectedRowKeys} 
+                                    selectedRows={this.state.selectedRows} 
+                                    table={this.state.table} 
+                                    data={this.state.tableData} 
+                                    loading={this.state.loading} 
+                                    resetData={this.resetData.bind(this)}/>
+    }
+
+}
